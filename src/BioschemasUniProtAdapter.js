9958f4ac590153b5fe10d56193971ef74189d2bf
ljgarcia <ljgarcia@ebi.ac.uk>
2018-06-12 10:35:16 +0100

    Move the jsonld rendering to this component

--- src/BioschemasUniProtAdapter.js
+++ src/BioschemasUniProtAdapter.js
@@ -12,6 +12,14 @@ export default class BioschemasUniProtAdapter extends ProtVistaUniProtEntryAdapt
 
     parseEntry(data) {
         this._adaptedData = this._parser.adaptData(data);
+        this._renderEntry();
         return this._adaptedData;
+    }
+
+    _renderEntry() {
+        const s = document.createElement('script');
+        s.type = 'application/ld+json';
+        s.innerHTML = JSON.stringify(this._adaptedData, null, 2);
+        document.body.appendChild(s);
     }
 }
